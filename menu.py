f53be8f7445fb22978f9de6cf32c8e6c9438c543
Santrugo99 <santiagotrujillogomez@gmail.com>
2026-07-28 21:46:07 -0500

    Clase #01

--- menu.py
+++ menu.py
@@ -1,3 +1,8 @@
+from funciones_menu import *
+
+lista_productos = []
+
+
 while True:
     print('--------------------------------')
     print('Menú')
@@ -6,18 +11,23 @@ while True:
     print('Opción 2: Eliminar Productos: ')
     print('Opción 3: Añadir Tipos De Productos: ')
     print('Opción 4: Listar Productos: ')
+    print('Opción 5: Verificar Existencia De Productos: ')
     print('--------------------------------')
 
     solicitud_info = input('Coloca la opción que desees: ')
     
     match solicitud_info:
         case "1": 
-            print('Usted ha elegido la opción 1')
+            producto_nuevo = input("Digita tu producto: ")
+            agregar_producto_a_lista(producto_nuevo,lista_productos)
         case "2":
-            print('Usted ha elegido la opción 2')
+            eliminar_producto = input("Digita tu producto: ")
+            eliminar_producto_de_la_lista(eliminar_producto,lista_productos)
+            print(f"se eliminó correctamente {lista_productos}")
         case "3":
             print('Usted ha elegido la opción 3')
         case "4":
-            print('Usted ha elegido la opción 4')
+            listar_productos(lista_productos)
         case _:
-            print("Usted no ha seleccionado una opción válida")    
+            print("Usted no ha seleccionado una opción válida")
+            
